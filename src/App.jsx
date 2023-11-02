741a836378768db8f0f4e22ec4d13cd3c1e2b61b
Thitisuda Mahasrithanet <thiti.m90@gmail.com>
2023-11-02 12:26:35 +0100

    add another h2

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Hej</h2>
+      <h2>Hej då</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
